7ca53298f744ef6b2da073ab04403ecb9d31802a
sudama-inc <53579774+sudama-inc@users.noreply.github.com>
2023-08-17 15:11:22 +0530

    Delete dsa_practice directory

--- dsa_practice/list/Sieve_of_Eratosthenes_method_to_compute_prime_nums.py
+++
@@ -1,9 +0,0 @@
-
-"""
-34. Write a Python program that uses the Sieve of Eratosthenes method to 
-compute prime numbers up to a specified number.
-
-Note: In mathematics, the sieve of Eratosthenes, one of a number of prime number sieves, 
-is a simple, ancient algorithm for finding all prime numbers up to any given limit.
-
-"""
--- dsa_practice/list/generate_all_permutations_of_list.py
+++
@@ -1,8 +0,0 @@
-
-"""
-***
-https://stackoverflow.com/questions/104420/how-do-i-generate-all-permutations-of-a-list
-
-Write a Python program to generate all permutations of a list in Python.
-
-"""
